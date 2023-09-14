1bb38b81cae63979f21f276fa16d649dc36e6ef1
web <cdavidsmall11@gmail.com>
2023-09-14 10:15:13 +0100

    Adding label name and timer

--- Facts.py
+++ Facts.py
@@ -2,4 +2,4 @@
 import requests
 import subprocess
 cat = requests.get("https://catfact.ninja/fact").json() ["fact"]
-subprocess.run(["/usr/bin/notify-send", "--icon=info", cat])
+subprocess.run(["/usr/bin/notify-send","--icon=info","-t","10000","-a","Cat Fact", cat])
